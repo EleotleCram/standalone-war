bf5977840bb59b09ca8cca6456cc543755cc1dc3
Marcel Toele <EleotleCram@gmail.com>
2012-05-31 14:58:58 +0200

    * Rewrote Starter almost completely so that it:  1) can be customized using gradle.properties,  2) supports ssl; * Added logging via log4j

--- starter/src/main/java/com/xlson/standalonewar/Starter.java
+++ starter/src/main/java/com/xlson/standalonewar/Starter.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2011 Leonard Axelsson
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,16 +11,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.xlson.standalonewar;
 
-import org.eclipse.jetty.server.Connector;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.security.ProtectionDomain;
+import java.util.List;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Starter for embedded Jetty. This class is intended to be packaged into a
@@ -35,41 +42,205 @@ import java.security.ProtectionDomain;
  * http://eclipsesource.com/blogs/2009/10/02/executable-wars-with-jetty/
  * <p/>
  * Example:
- * java -jar -Dport=80 webapp.war
+ * java -jar -Dwebserver.listenPort=80 webapp.war
  * <p/>
- * webapp.war is loaded on http://localhost:8080/
+ * webapp.war is loaded on http://localhost/
  *
  * @author Leonard Axelsson
+ * @author Joris Koster
+ * @author Marcel Toele
  */
+@SuppressWarnings({"unchecked","deprecation"}) // Tell the compiler to shut up: the old API is simpler and better!
 public class Starter {
 
-  private static final int DEFAULT_PORT = 8080;
+	private static Server server;
+	private static PropertiesConfiguration defaultProperties;
+	private static PropertiesConfiguration config;
+	private static Logger logger = LoggerFactory.getLogger(Starter.class);
 
-  public static void main(String[] args) throws Exception {
-    Server server = new Server();
-    SocketConnector connector = new SocketConnector();
-    connector.setMaxIdleTime(1000 * 60 * 60);
-    connector.setSoLingerTime(-1);
-    connector.setPort(Integer.getInteger("port", DEFAULT_PORT));
-    server.setConnectors(new Connector[]{connector});
+	private static String getString(String propertyName) {
+		return getString(propertyName, null);
+	}
 
-    WebAppContext context = new WebAppContext();
-    context.setServer(server);
-    context.setContextPath(System.getProperty("app.context", "/"));
+	private static String getString(String propertyName, String defaultValue) {
+		return System.getProperty(propertyName, config.getString(propertyName, defaultValue));
+	}
 
-    ProtectionDomain protectionDomain = Starter.class.getProtectionDomain();
-    URL location = protectionDomain.getCodeSource().getLocation();
-    context.setWar(location.toExternalForm());
+	private static Integer getInteger(String propertyName, Integer defaultValue) {
+		return Integer.getInteger(propertyName, config.getInteger(propertyName, defaultValue));
+	}
 
-    server.setHandler(context);
-    try {
-      server.start();
-      System.in.read();
-      server.join();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.exit(100);
-    }
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			defaultProperties = new PropertiesConfiguration(Starter.class.getClassLoader().getResource("webserver.properties").toURI().toURL());
 
-  }
+			appendClasspath(defaultProperties.getList("webserver.extraClasspath"));
+
+			String configFilename = defaultProperties.getString("webserver.configFilename", "webserver.conf");
+			try {
+				config = new PropertiesConfiguration(configFilename);
+			} catch(ConfigurationException e) {
+				config = new PropertiesConfiguration();
+			}
+
+			final String PROP_NAME_WEBSERVER_TIMEOUT = defaultProperties.getString("PROP_NAME_WEBSERVER_TIMEOUT", "webserver.timeout");
+			int timeout = Integer.parseInt(getString(PROP_NAME_WEBSERVER_TIMEOUT, "0"));
+			start();
+			if (timeout != 0) {
+				Thread.sleep(timeout);
+				server.stop();
+				server.join();
+			}
+		} catch (Exception ex) {
+			logger.error("error", ex);
+			System.err.println(ex.getMessage());
+			System.exit(-1);
+		}
+	}
+
+	public static void start() throws Exception {
+		final String PROP_NAME_WEBSERVER_BIND_ADDRESS = defaultProperties.getString("PROP_NAME_WEBSERVER_BIND_ADDRESS", "webserver.bindAddress");
+		final String PROP_NAME_WEBSERVER_LISTENPORT = defaultProperties.getString("PROP_NAME_WEBSERVER_LISTENPORT", "webserver.listenPort");
+		final String PROP_NAME_WEBSERVER_TEMPDIR = defaultProperties.getString("PROP_NAME_WEBSERVER_TEMPDIR", "webserver.tempDir");
+		final String PROP_NAME_WEBSERVER_SSL = defaultProperties.getString("PROP_NAME_WEBSERVER_SSL", "webserver.ssl");
+		final String PROP_NAME_WEBSERVER_SSL_LISTENPORT = defaultProperties.getString("PROP_NAME_WEBSERVER_SSL_LISTENPORT", "webserver.ssl.listenPort");
+		final String PROP_NAME_WEBSERVER_SSL_KEYSTORE_TYPE = defaultProperties.getString("PROP_NAME_WEBSERVER_SSL_KEYSTORE_TYPE", "webserver.ssl.keyStoreType");
+		final String PROP_NAME_WEBSERVER_SSL_KEYSTORE_FILE = defaultProperties.getString("PROP_NAME_WEBSERVER_SSL_KEYSTORE_FILE", "webserver.ssl.keyStoreFile");
+		final String PROP_NAME_WEBSERVER_SSL_KEYSTORE_PASSWORD = defaultProperties.getString("PROP_NAME_WEBSERVER_SSL_KEYSTORE_PASSWORD", "webserver.ssl.keyStorePassword");
+
+		String tempPath = getString(PROP_NAME_WEBSERVER_TEMPDIR, System.getProperty("java.io.tmpdir"));
+		logger.info("using '" + tempPath + "' as temporary directory");
+
+		server = new Server();
+
+		String bindAddress = getString(PROP_NAME_WEBSERVER_BIND_ADDRESS, "0.0.0.0");
+
+		if ("true".equalsIgnoreCase(getString(PROP_NAME_WEBSERVER_SSL))) {
+			/* Note: the default keystore file type for war-launcher is PKCS12, you create a PKCS12 key store file with:
+			 *
+			 *  1) OpenSSL: openssl pkcs12 -export -clcerts -in example.com.crt -inkey example.com.key -out example.com.p12
+			 *  2) keytool: @TODO
+			 */
+			String keyStoreType = getString(PROP_NAME_WEBSERVER_SSL_KEYSTORE_TYPE, "PKCS12");
+			logger.info("using '" + keyStoreType + "' as key store type");
+
+			String keyStoreFile = getString(PROP_NAME_WEBSERVER_SSL_KEYSTORE_FILE, null);
+			if (keyStoreFile == null) {
+				throw new RuntimeException("ssl requires a property value for: " + PROP_NAME_WEBSERVER_SSL_KEYSTORE_FILE);
+			} else {
+				logger.info("using '" + keyStoreFile + "' as key store file");
+			}
+
+			String keyStorePassword = getString(PROP_NAME_WEBSERVER_SSL_KEYSTORE_PASSWORD);
+
+			int defaultSslListenPort = Integer.parseInt(defaultProperties.getString("webserver.ssl.defaultListenPort", "8443"));
+			String sslListenPort = getString(PROP_NAME_WEBSERVER_SSL_LISTENPORT, null);
+			int sslPort;
+			if (sslListenPort == null) {
+				logger.warn("'" + PROP_NAME_WEBSERVER_SSL_LISTENPORT + "' was not specified, using the default port.");
+				sslPort = defaultSslListenPort;
+			} else {
+				sslPort = Integer.decode(sslListenPort);
+				logger.info("using port '" + sslPort + "' for ssl connections");
+			}
+
+			SslSelectChannelConnector connector = new SslSelectChannelConnector();
+			try {
+				connector.setKeystoreType(keyStoreType);
+				connector.setKeystore(keyStoreFile);
+				connector.setPassword(keyStorePassword);
+				connector.setKeyPassword(keyStorePassword);
+				connector.setHost(bindAddress);
+				connector.setPort(sslPort);
+
+				logger.info("Adding HTTPS connector for " + bindAddress + ":" + sslPort);
+				server.addConnector(connector);
+			} catch (Exception ex) {
+				logger.error("error", ex);
+			}
+		}
+
+		int defaultListenPort = Integer.parseInt(defaultProperties.getString("webserver.defaultListenPort", "8080"));
+		String listenPort = getString(PROP_NAME_WEBSERVER_LISTENPORT, null);
+		if ((listenPort != null) || (server.getConnectors() == null)) {
+			SocketConnector connector = new SocketConnector();
+
+			if (listenPort != null) {
+				logger.warn("'" + PROP_NAME_WEBSERVER_LISTENPORT + "' was not specified, using the default port.");
+			}
+
+			int port = getInteger(PROP_NAME_WEBSERVER_LISTENPORT, defaultListenPort);
+			connector.setHost(bindAddress);
+			connector.setPort(port);
+			connector.setMaxIdleTime(1000 * 60 * 60);
+			connector.setSoLingerTime(-1);
+
+			logger.info("Adding HTTP connector for " + bindAddress + ":" + port);
+			server.addConnector(connector);
+		} else {
+			logger.warn("'" + PROP_NAME_WEBSERVER_LISTENPORT
+					+ "' was not specified, but other connectors already registered, so no default connector was added for port "
+					+ defaultListenPort);
+		}
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setExtraClasspath(getString("webserver.extraClasspath", ""));
+		webapp.setContextPath("/");
+		webapp.setTempDirectory(new File(tempPath));
+		webapp.setServer(server);
+
+		ProtectionDomain protectionDomain = Starter.class.getProtectionDomain();
+		URL location = protectionDomain.getCodeSource().getLocation();
+		webapp.setWar(location.toExternalForm());
+
+		server.setHandler(webapp);
+		server.start();
+	}
+
+	public static void stop() throws Exception {
+		if (server != null) {
+			server.stop();
+			server.join();
+		}
+	}
+
+	/**
+	 * Add paths to the system class loader.
+	 * (The paths are expected to be comma-separated.)
+	 */
+	public static void appendClasspath(List<String> paths) throws IOException {
+		for(String path : paths) {
+			path = path.replaceAll("~", System.getProperty("user.home")) + File.separator;
+			URL url;
+			try {
+				url = new URL(path);
+			} catch(MalformedURLException e) {
+				url = new URL("file://"+path);
+			}
+			logger.info("Appending the following path to the system class path: " + url);
+			appendClasspath(url);
+		}
+	}
+	private static final Class[] parameters = new Class[]{URL.class};
+	/**
+	 * Add url to the system class loader.
+	 */
+	public static void appendClasspath(URL url) throws IOException {
+
+		URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		Class sysclass = URLClassLoader.class;
+
+		try {
+			Method method = sysclass.getDeclaredMethod("addURL", parameters);
+			method.setAccessible(true);
+			method.invoke(sysloader, new Object[]{url});
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw new IOException("Error, could not add URL to system classloader");
+		}
+
+	}
 }
